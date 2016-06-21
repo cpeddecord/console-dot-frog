7a71bd21d760e61bc691cd1ccb675963e6232827
Chris Peddecord <cpeddecord@gmail.com>
2016-06-21 12:45:33 -0700

    added rainbow styling

--- console.frog.js
+++ console.frog.js
@@ -12,8 +12,14 @@
     // Well... nothing really... let it be!
     return;
   }
+  
+  var rainbowStyles = "background-image: -webkit-gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );" + 
+  "background-image: gradient( linear, left top, right top, color-stop(0, #f22), color-stop(0.15, #f2f), color-stop(0.3, #22f), color-stop(0.45, #2ff), color-stop(0.6, #2f2),color-stop(0.75, #2f2), color-stop(0.9, #ff2), color-stop(1, #f22) );" + 
+  "color:transparent;" +
+  "-webkit-background-clip: text;" +
+  "background-clip: text;";
 
-  window.console.frog = function() {
+  window.console.rainbowfrog = function() {
 
     var i;
     
@@ -40,7 +46,8 @@
 
     // Log the frog!
     for( i = 0; i < frog.length; i++ ) {
-      console.log(frog[i] + "\n");
+      
+      console.log("%c" + frog[i] + "\n", rainbowStyles);
     }
   }
 
